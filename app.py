72590488a26482d2b8a5c30ec21b8d7edaea58cf
kemo <kemo617@163.com>
2024-07-09 11:36:53 +0800

    Add index page codes

--- app.py
+++ app.py
@@ -1,9 +1,24 @@
 from flask import Flask
 from flask import url_for
+from flask import render_template
 
 app = Flask(__name__)
 
+name = 'kemo'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
+
+@app.route('/hello')
 def hello():
     return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
 
@@ -16,6 +31,7 @@ def test_url_for():
     print(url_for('hello'))
     print(url_for('user_page', name='kemo'))
     print(url_for('test_url_for'))
+    print(url_for('test_url_for', num=2))
     return 'Test page'
 
 if __name__ == '__main__':

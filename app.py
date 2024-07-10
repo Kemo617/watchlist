56963d1d89c3ecadebd74771e20862bc32375049
kemo <kemo617@163.com>
2024-07-10 17:29:39 +0800

    User authentication with Flask-Login

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ import sys
 import click
 
 from flask import Flask, url_for, request, redirect, flash, render_template
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -17,19 +19,41 @@ app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'da
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # 设置签名所需的密钥
 app.config['SECRET_KEY'] = 'dev'
-# 初始户扩展,传入程序实例app
+# 初始化扩展,传入程序实例app
 db = SQLAlchemy(app)
+# 登陆管理类实例化
+login_manager = LoginManager(app)
+# 登录视图为起点
+login_manager.login_view = 'showlogin'
 
-class User(db.Model):
+# ----------------- 用户加载回调函数 -----------------
+@login_manager.user_loader
+def load_user(user_id):
+    user = User.query.get(int(user_id))
+    return user
+
+# ----------------- 数据模型 -----------------------
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(20))
+    ownername = db.Column(db.String(20))
+    username = db.Column(db.String(20))
+    password_hash = db.Column(db.String(128))
+
+    # 设置口令
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    # 验证口令
+    def validate_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
 
-# 注册为命令, 初始化数据库
+# ----------------- 注册命令 -----------------------
+# 初始化数据库
 @app.cli.command() 
 @click.option('--drop', is_flag=True, help='Create after drop.')
 def initdb(drop):
@@ -38,12 +62,48 @@ def initdb(drop):
     db.create_all()
     click.echo('Initialized database.')
 
-# 注册为命令, 向数据库填充示例数据
+# 创建管理用户
 @app.cli.command()
-def forge():
+@click.option('--username', prompt=True, help='The username used to login.')
+@click.option('--password', prompt=True, hide_input=True, confirmation_prompt=True, help='The password used to login.')
+def setadmin(username, password):
     db.create_all()
 
-    name = 'Kemo'
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating user ...')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('Creating user ...')
+        user = User(username=username)
+        user.set_password(password)
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo('Done.')
+
+# 设置所有者名
+@app.cli.command()
+@click.option('--ownername', prompt=True, help='The ownername of watchlist.')
+def setowner(ownername):
+    db.create_all()
+
+    user = User.query.first()
+    if user is not None:
+        click.echo('Updating owner ...')
+        user.ownername = ownername
+    else:
+        click.echo('Creating owner ...')
+        user = User(ownername=ownername)
+        db.session.add(user)
+
+    db.session.commit()
+    click.echo('Done.')
+
+# 向数据库填充电影示例数据
+@app.cli.command()
+def forgemovies():
     movies = [
         {'title': 'My Neighbor Totoro', 'year': '1988'},
         {'title': 'Dead Poets Society', 'year': '1989'},
@@ -53,8 +113,6 @@ def forge():
         {'title': 'The Pork of Music', 'year': '2012'}
     ]
 
-    user = User(name=name)
-    db.session.add(user)
     for m in movies:
         movie = Movie(title=m['title'], year=m['year'])
         db.session.add(movie)
@@ -68,8 +126,57 @@ def forge():
 def index():
     return render_template('index.html')
 
+# 显示登录页面
+@app.route('/login')
+def showlogin():
+    return render_template('login.html')
+
+# 显示设置页面
+@app.route('/settings')
+def showsettings():
+    return render_template('settings.html')
+
+# 登入
+@app.route('/loginA', methods=['POST'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    user = User.query.first()
+    if username == user.username and user.validate_password(password):
+        login_user(user)
+        flash('Login success.')
+    else:
+        flash('Invalid username or password.')
+        return redirect(url_for('showlogin'))
+        
+    return redirect(url_for('index'))
+
+# 登出
+@app.route('/logout')
+@login_required # 视图保护
+def logout():
+    logout_user()
+    flash('Goodbye.')
+    return redirect(url_for('index'))
+
+# 设置
+@app.route('/settingsA', methods=['POST'])
+@login_required 
+def settings():
+    ownername = request.form['ownername']
+    if not ownername or len(ownername) > 20:
+        flash('Invalid input.')
+    else:
+        current_user.ownername = ownername
+        db.session.commit()
+        flash('Settings updated.')
+        
+    return redirect(url_for('showsettings'))
+
 # 增加记录
 @app.route('/movie/add', methods=['POST'])
+@login_required 
 def add():
     title = request.form.get('title')
     year = request.form.get('year')
@@ -87,6 +194,7 @@ def add():
 
 # 打开编辑
 @app.route('/movie/edit', methods=['POST'])
+@login_required 
 def edit():
     movie_id = request.form['movie_id']
     movie = Movie.query.get_or_404(movie_id)
@@ -94,6 +202,7 @@ def edit():
 
 # 更新记录
 @app.route('/movie/update', methods=['POST'])
+@login_required 
 def update():
     title = request.form['title']
     year = request.form['year']
@@ -114,6 +223,7 @@ def update():
 
 # 删除记录
 @app.route('/movie/delete', methods=['POST'])
+@login_required 
 def delete():
     movie_id = request.form['movie_id']
     movie = Movie.query.get_or_404(movie_id)

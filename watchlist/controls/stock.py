74cda7d9fb740837c63fb18bdcb02f3569cb4309
kemo <kemo617@163.com>
2024-07-15 01:25:50 +0800

    For test akshare

--- watchlist/controls/stock.py
+++ watchlist/controls/stock.py
@@ -1,4 +1,4 @@
-import efinance as ef
+import akshare as ak
 from flask import flash
 
 # ...
@@ -16,7 +16,17 @@ def get_stock_info(stockcode):
     stockinfo = None
     try:
         # 通过股票代码获取个股信息
-        lastdeal = ef.stock.get_deal_detail(stockcode).tail(1)
+        # 获取个股基本信息
+        stockinfo = StockInfo()
+        stockinfo.stockname = ak.stock_individual_info_em(symbol=stockcode).loc[1, "value"]
+        stockinfo.stockcode = stockcode
+        stockinfo.pricenow = 0.0
+        stockinfo.priceyesterday = 0.0
+        
+        '''
+        # 取当前日期, 回溯10天, 取最后一条, 得到价格
+        stock_df = ak.stock_zh_a_spot_em(symbol=stockcode)
+        lastdeal = ak.stock.get_deal_detail(stockcode).tail(1)
         linelabel = lastdeal.index.start
         if stockcode == lastdeal.loc[linelabel, '股票代码']:
             stockinfo = StockInfo()
@@ -24,6 +34,7 @@ def get_stock_info(stockcode):
             stockinfo.stockname = lastdeal.loc[linelabel, '股票名称']
             stockinfo.pricenow = lastdeal.loc[linelabel, '成交价']
             stockinfo.priceyesterday = lastdeal.loc[linelabel, '昨收']
+        '''
     except BaseException as e:
         flash(f"获取股票信息异常 --> {e}")
 
